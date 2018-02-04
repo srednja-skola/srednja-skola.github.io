a24ecaeeaa64c34e7b445bb83e19327be58ea5b5
srednja-skola <dgavrano@gmail.com>
2018-02-04 16:50:50 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var pokrivac = document.querySelector(".pokrivac");
+
+function sifra() {
+    var unos = prompt("Upiši šifru kako bi vidio sadržaj");
+    if (unos === null || unos === undefined || unos === "") {
+       pokrivac.classList = "pokrivac";
+    } else if (unos === "demogeografija") {
+        pokrivac.classList = "";
+    } else {
+        pokrivac.classList = "pokrivac";
+
+    }
+}
+sifra();
