14c4f43d2b7f5958793ed1f10176f4ab031867f2
srednja-skola <dgavrano@gmail.com>
2018-03-03 13:33:53 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,14 +1,43 @@
+// footer copyleft (nova godina se mjenja automatski)
 // encrypt
-var pokrivac = document.querySelector(".pokrivac");
+var pokrivac = document.querySelector(".pokrivac"),
+  copyleft = document.querySelector(".copyleft");
 
-function sifra() {
+// rute za pojedinu stranicu
+
+var route = {
+  _routes: {}, // The routes will be stored here
+  add: function(url, action) {
+    this._routes[url] = action;
+  },
+  run: function() {
+    jQuery.each(this._routes, function(pattern) {
+      if (location.href.match(pattern)) {
+        // "this" points to the function to be executed
+        this();
+      }
+    });
+  }
+}
+// aktiviraj na svim stranicama (ostaviš prazno i to znači svi linkovi) - sve one stranice na kojima želiš drukčije, postavi ispod ovog koda
+route.add("", function() {
+  // footer s linkom na mom imenu
+  copyleft.innerHTML = "Copyleft <span class='cl'>&ensp;&copy;&ensp;</span> " + new Date().getFullYear() + ". - Danijel Gavranović, prof.";
+});
+route.add("dodatna-nastava.html", function() {
+  // footer s linkom na mom imenu
+  function sifra() {
     var unos = prompt("Upiši šifru kako bi vidio sadržaj");
     if (unos === null || unos === undefined || unos === "") {
-       pokrivac.classList = "pokrivac";
+      pokrivac.classList = "pokrivac";
     } else if (unos === "demogeografija") {
-        pokrivac.classList = "";
+      pokrivac.classList = "";
     } else {
-        pokrivac.classList = "pokrivac";
+      pokrivac.classList = "pokrivac";
     }
-}
-sifra();
+  }
+  sifra();
+});
+
+// pozivanje route funkcije
+route.run();
