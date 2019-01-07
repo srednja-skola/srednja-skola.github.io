c152dbfccf4701b602770a0d30886766c1cd488f
srednja-skola <dgavrano@gmail.com>
2019-01-07 12:22:44 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -37,7 +37,20 @@ route.add("dodatna-nastava.html", function() {
     }
   }
   sifra();
+}),
+route.add("migracije.html", function() {
+  // footer s linkom na mom imenu
+  function sifra() {
+    var unos = prompt("Upiši šifru kako bi vidio sadržaj");
+    if (unos === null || unos === undefined || unos === "") {
+      pokrivac.classList = "pokrivac";
+    } else if (unos === "demogeografija") {
+      pokrivac.classList = "";
+    } else {
+      pokrivac.classList = "pokrivac";
+    }
+  }
+  sifra();
 });
-
 // pozivanje route funkcije
 route.run();
